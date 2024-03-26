714208249817bfff96ca0d451905ad679f9387cf
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2024-03-26 13:15:54 +0100

    test

--- src/pages/Available.tsx
+++ src/pages/Available.tsx
@@ -3,14 +3,14 @@ import GroupedTable from "../components/GroupedTable";
 import { Reward } from "../types/Reward";
 import ItemsTable from "../components/ItemsTable";
 import { fetchData } from "../utils/fetchData";
-// import {
-//   browserName,
-//   browserVersion,
-//   deviceType,
-//   isMobile,
-//   isTablet,
-//   isDesktop,
-// } from "react-device-detect";
+import {
+  browserName,
+  browserVersion,
+  deviceType,
+  isMobile,
+  isTablet,
+  isDesktop,
+} from "react-device-detect";
 
 const Available = () => {
   const [currentlyAvailableRelics, setCurrentlyAvailableRelics] = useState([]);
@@ -47,6 +47,11 @@ const Available = () => {
 
   return (
     <div>
+      <p>
+        BrowserName: {browserName}, BrowserVersion: {browserVersion}, Device
+        Type: {deviceType}
+        is mobile: {isMobile}, is tablet: {isTablet}, is desktop: {isDesktop}
+      </p>
       <h1>Warframe Relics</h1>
       <div className="card">
         {isFetching && <h1>Loading...</h1>}
